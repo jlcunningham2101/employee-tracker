941da13d2b93fa83062aee8215bba46b5d9780e0
Jill Cunningham <jleighcunningham@gmail.com>
2022-02-17 22:14:42 -0500

    saved queries, created seed files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,3 @@
-CREATE TABLE department (
-id INT PRIMARY KEY,       
-name VARCHAR(30) NOT NULL
-);
-
-CREATE TABLE role (
-id INT PRIMARY KEY,
-title VARCHAR(30) NOT NULL,
-salary DECIMAL NOT NULL,
-department_id INT NOT NULL
-);
-
-CREATE TABLE employees (
-id INT AUTO_INCREMENT PRIMARY KEY,
-first_name VARCHAR(30) NOT NULL,
-last_name VARCHAR(30) NOT NULL,
-role_id INT NOT NULL,
-manager_id INT NULL
-);
-
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES      
 ('Mike', 'Chan', 1234, 4321), 
@@ -27,5 +7,3 @@ VALUES
 ('Malia', 'Brown', 1238, 8321),
 ('Sarah', 'Lourd', 1239, 9321),
 ('Tom', 'Allen', 1240, 0421);
-
-
