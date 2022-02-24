7333bfe236a5732bc5a8c27f3d4c0dfe72bdc33c
Jill Cunningham <jleighcunningham@gmail.com>
2022-02-24 00:01:33 -0500

    started to build functions for user input received

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
 const inquirer = require('Inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
+const Connection = require('mysql2/typings/mysql/lib/Connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -20,7 +21,7 @@ app.use((req, res) => {
   res.status(404).end();
 });
 
-// Start server after DB connection
+// Start server after database connection
 db.connect(err => {
   if (err) throw err;
   console.log('Database connected.');
@@ -36,34 +37,64 @@ const promptUser = () => {
     type: "list",
     name: "start page",
     message: "How would you like to proceed? Please click on one of the options below:",
-    choices:['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add An Employee', 'Update an Employee Role'],
-    validate: startInput => {
-      if (startInput) {
-          return true;
-        } else {
-          console.log('Please identify if you want to view, add, or update company details.');
-          return false;
-        }
-      }
-    },
+    choices:['View All Departments', 'View All Roles', 'View All Employees', 'Add Department', 'Add Role', 'Add an Employee', 'Update an Employee Role'],
+    }
 
-// TODO: Create a function to write company-details file
-function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data, err => {
-  if (err) throw err;
-  console.log('company-details are complete')
-});
-  
+.then (function(res) {
+switch (res.startPage){
+case "View All Departments":
+viewDepartments();
+break;
 
-// TODO: Create a function to initialize app
-function init() {
-promptUser()
-.then(function(data){
-  console.log('inside.then')
-  console.log(data)
-    writeToFile("company-details.md", generateMarkdown(data));
+case "View All Roles":
+viewRoles();
+break;
+
+case "View All Employees":
+viewEmployees();
+break;
+
+case "Add Department":
+addDepartment();
+break;
+
+case "Add an Employee":
+  addEmployee();
+  break;
+
+case "Update an Employee":
+  UpdateEmployee();
+  break;
+  }
 });
 }
+
+function viewDepartments(){
+  let query = "SELECT department.name";
+
+}
+
+function viewEmployees(){
+let query = "SELECT employees.first_name, employees.last_name";
+query += "FROM employees";
+query += "INNER JOIN role ON role.department_id = role.department_id";
+query += "LEFT JOIN employees ON employees.manager_id = manager.id";
  
+connection.query(query, function(err, res){
+  console.table('All Employees',res);
+})
+)
+}
+
+
+
+
+
+
+
+
+
+
+
 // Function call to initialize app
-//();
+init();
